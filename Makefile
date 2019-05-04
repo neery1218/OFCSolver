1d45678ecce3b24cb090abc175131a21ff735f2a
neerajensritharan <neery1218@gmail.com>
2019-05-04 00:50:27 -0400

    hello world server

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-INC = -I /usr/local/include/boost/
+INC = -I /usr/local/include/boost/ -I include/
 SRCS = hand.cc solver.cc card.cc pull.cc deck.cc poker_hand_evaluator.cc decision_finder.cc fantasy_solver.cc
 DRIVER = main.cc
 TESTS = test_hand.cc test_decision_finder.cc
+CXX = g++
+CFLAGS = -std=c++17 -Wall -O3
+
 all: 
 	g++ -std=c++17 -Wall -O2 $(INC) $(SRCS) $(DRIVER) -o main -lpthread
 
@@ -15,7 +18,7 @@ profile:
 	g++ -pg -O3 -std=c++17 $(INC) $(SRCS) $(DRIVER) -o mainprof
 
 clean:
-	rm -f main maindebug mainprof maintest
+	rm -f main maindebug mainprof maintest server
 
 test_main.o: test_main.cc
 	g++ -std=c++17 -O3 test_main.cc -c
@@ -25,3 +28,6 @@ fantasysolver: all
 
 fantasysolverdebug:
 	g++ -g -std=c++17 -O3 $(INC) $(SRCS) fantasy_simulator.cc -o fantasysolverdebug
+
+server: server.cc
+	$(CXX) $(CFLAGS) $(INC) $(SRCS) server.cc -o server -lpthread
---
+++ server.cc
@@ -0,0 +1,155 @@
+#include <boost/algorithm/string.hpp>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <set>
+#include <utility>
+#include "hand.h"
+#include "pull.h"
+#include "gametype.h"
+#include "solver.h"
+#include "card.h"
+#include "position.h"
+#include "decision.h"
+#include "placement.h"
+#include "decision_finder.h"
+#include <fstream>
+#include <cassert>
+#include <httplib.h>
+
+using namespace std;
+
+set<Card> parseCards(string cards) {
+  vector<string> tokens;
+  set<Card> parsedCards;
+
+  boost::split(tokens, cards, boost::is_any_of(" "));
+  for (auto token : tokens) {
+    assert(token.size() == 2);
+    parsedCards.insert(Card(token));
+  }
+
+  return parsedCards;
+}
+
+int main(int argc, char *argv[]) {
+  using namespace httplib;
+
+  Server svr;
+
+  svr.Get("/eval", [] (const Request & req, Response &res) {
+      PokerHandEvaluator evaluator(GameType::Progressive);
+      evaluator.
+      res.set_content("Hello World!", "text/plain");
+  });
+  svr.listen("localhost", 8080);
+  /*
+  int command;
+  char game_mode;
+  GameType type;
+
+  cout << "Game mode? (r, p, u) : ";
+  cin >> game_mode;
+
+  if (game_mode == 'r') type = GameType::Regular;
+  else if (game_mode == 'p') type = GameType::Progressive;
+  else if (game_mode == 'u') type = GameType::Ultimate;
+  else throw "wtf";
+
+  PokerHandEvaluator evaluator(type);
+
+  cout << "Enter number of players : ";
+  cin >> command;
+
+  while (command) {
+    ifstream f("input.txt");
+    string line;
+
+    // parse pull
+    getline(f, line);
+    Pull my_pull{parseCards(line)};
+    cout << "My pull :" << my_pull << "\n\n";
+
+    getline(f, line);
+
+    // parse my hand
+    getline(f, line);
+    set<Card> m_top = line == "x" ? set<Card>() : parseCards(line);
+
+    getline(f, line);
+    set<Card> m_mid = line == "x" ? set<Card>() : parseCards(line);
+
+    getline(f, line);
+    set<Card> m_bot = line == "x" ? set<Card>() : parseCards(line);
+
+    Hand my_hand(m_top, m_mid, m_bot);
+    cout << "My hand: \n" << my_hand << "\n\n";
+
+    vector<Hand> other_hands;
+    for (int i = 0; i < command - 1; ++i) {
+      getline(f, line);
+
+      getline(f, line);
+      set<Card> o_top = line == "x" ? set<Card>() : parseCards(line);
+
+      getline(f, line);
+      set<Card> o_mid = line == "x" ? set<Card>() : parseCards(line);
+
+      getline(f, line);
+      set<Card> o_bot = line == "x" ? set<Card>() : parseCards(line);
+      
+      if (o_top.size() + o_mid.size() + o_bot.size() > 0) {
+        other_hands.push_back(Hand(o_top, o_mid, o_bot));
+        cout << "Other hand " << i + 1 << " : \n" << other_hands[i] << "\n\n";
+      }
+    }
+
+    getline(f, line);
+
+    // parse dead cards
+    getline(f, line);
+    vector<Card> dead_cards;
+    if (line != "x") {
+      set<Card> tmp = parseCards(line);
+      dead_cards.insert(dead_cards.end(), tmp.begin(), tmp.end());
+    }
+    cout << dead_cards.size() << " dead cards. \n";
+
+    // evaluate decisions
+    // set solver
+    GameState game_state{my_hand, other_hands, my_pull, dead_cards};
+    Decision d = DecisionFinder(evaluator).findBestDecision(game_state);
+    cout << "Best decision is: " << d << "\n";
+
+    cout << "Enter number of players : ";
+    cin >> command;
+    if (command == 0) return 0;
+  }
+  */
+  // create ofc hands
+  /*
+  Hand myHand(
+      parseCards("Ac"), 
+      parseCards("2c 2d"), 
+      parseCards("9h 9d 7d 7s"));
+
+  Hand otherHand(
+      parseCards("As Qc Qd"), 
+      parseCards("3c 3d 3s"), 
+      parseCards("Th Td Ts"));
+
+  Pull myPull = {parseCards("Ah 2s 9s")};
+
+  vector<Hand> otherHands = {otherHand};
+  vector<Card> dead_cards = {Card("Kd")};
+
+  Decision d = DecisionFinder(GameType::Regular).findBestDecision(myHand, myPull, otherHands, dead_cards);
+  */
+  /*
+  Decision d = SetDecisionFinder(GameType::Progressive).findBestDecision(
+      Pull{parseCards("Qd 4s 5s 9s As")},
+      vector<Hand> ());
+  */
+}
